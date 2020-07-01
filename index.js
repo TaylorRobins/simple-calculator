489f9d6f5e584916ce9b8cf21cf1cd680fa14216
Taylor Robins <robinsnom@gmail.com>
2020-06-30 20:38:38 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+// sudo code
+
+// Grab each of the elements
+// from the 'document'
+// and store them in variables
+// -left input
+// -right input
+// -add button
+// -substract button
+// -multiply button
+// -divide button
+// -result
+
+// add event listeners to each
+// math operator buttons
+
+//  Create 4 functions- one for each
+//  math operator
+
+function add(leftValue, rightValue) {
+  const result = leftValue + rightValue;
+
+  return result;
+}
+
+function minus(leftValue, rightValue) {
+  const result = leftValue - rightValue;
+
+  return result;
+}
+
+function multiply(leftValue, rightValue) {
+  const result = leftValue * rightValue;
+
+  return result;
+}
+
+function divide(leftValue, rightValue) {
+  const result = leftValue / rightValue;
+
+  return result;
+}
