4cb5f4c278ed3ae8b1af303057599d1e580ec119
Taylor Robins <robinsnom@gmail.com>
2020-07-01 20:12:57 -0600

    finished calculator in js

--- index.js
+++ index.js
@@ -12,8 +12,72 @@ console.log("Hello World");
 // -divide button
 // -result
 
+const leftInputElement = document.getElementById("leftInput");
+const rightInputElement = document.getElementById("rightInput");
+const addButtonElement = document.getElementById("addButton");
+const minusButtonElement = document.getElementById("minusButton");
+const multiplyButtonElement = document.getElementById("multiplyButton");
+const divideButtonElement = document.getElementById("divideButton");
+const resultElement = document.getElementById("result");
+
 // add event listeners to each
 // math operator buttons
+
+addButtonElement.addEventListener("click", function () {
+  // grab left input value
+  const leftValue = Number(leftInputElement.value);
+
+  // grab right input value
+  const rightValue = Number(rightInputElement.value);
+
+  //add the two -> call our add() function with the two values
+  const result = add(leftValue, rightValue);
+
+  // set the result into the result <span>
+  resultElement.innerText = result;
+});
+
+minusButtonElement.addEventListener("click", function () {
+  // grab left input value
+  const leftValue = Number(leftInputElement.value);
+
+  // grab right input value
+  const rightValue = Number(rightInputElement.value);
+
+  //add the two -> call our add() function with the two values
+  const result = minus(leftValue, rightValue);
+
+  // set the result into the result <span>
+  resultElement.innerText = result;
+});
+
+multiplyButtonElement.addEventListener("click", function () {
+  // grab left input value
+  const leftValue = Number(leftInputElement.value);
+
+  // grab right input value
+  const rightValue = Number(rightInputElement.value);
+
+  //add the two -> call our add() function with the two values
+  const result = multiply(leftValue, rightValue);
+
+  // set the result into the result <span>
+  resultElement.innerText = result;
+});
+
+divideButtonElement.addEventListener("click", function () {
+  // grab left input value
+  const leftValue = Number(leftInputElement.value);
+
+  // grab right input value
+  const rightValue = Number(rightInputElement.value);
+
+  //add the two -> call our add() function with the two values
+  const result = divide(leftValue, rightValue);
+
+  // set the result into the result <span>
+  resultElement.innerText = result;
+});
 
 //  Create 4 functions- one for each
 //  math operator
@@ -41,3 +105,14 @@ function divide(leftValue, rightValue) {
 
   return result;
 }
+
+// TEST FUNCTIONS
+
+const leftValue = 15;
+
+const rightValue = 3;
+
+console.log(add(leftValue, rightValue));
+console.log(minus(leftValue, rightValue));
+console.log(multiply(leftValue, rightValue));
+console.log(divide(leftValue, rightValue));
